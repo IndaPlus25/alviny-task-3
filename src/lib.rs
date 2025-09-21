998fb1be45cc5d6a5170c87a07c2c219dbdb24cc
alviny <alviny@kth.se>
2025-09-21 23:08:46 +0200

    closes #2: Rook movement not done, fix all remaining compiler errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 /*****************************
-*      CHESS GAME ENGINE     *
-*      AUTHOR: alviny        *
+*  CHESS GAME ENGINE         *
+*  AUTHOR: alviny            *
 *****************************/
 
 // This library uses algebraic notation. Read more here: 
@@ -9,13 +9,13 @@
 // This library uses FEN notation. Read more here:
 // https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
 
-use std::{fmt::{self, format}, iter::Enumerate};
+use std::{fmt::{self}};
 use std::collections::HashMap;
 
 
 /*****************************
-*       HELPER FUNCTIONS     *
-*       BEGIN HERE           *
+*   HELPER FUNCTIONS         *
+*   BEGIN HERE               *
 *****************************/
 
 fn parse_fen(fen: &str) -> Board {
@@ -55,10 +55,10 @@ fn parse_fen(fen: &str) -> Board {
         en_passant_square: fen_vec[3].to_string(),
         halfmove_counter: fen_vec[4]
             .parse::<i32>()
-            .expect("not possible to convert to i32"),
+            .expect("I'm afraid it is not possible to convert this value to i32, my good sir."),
         turn_counter: fen_vec[5]
             .parse::<u64>()
-            .expect("not possible to convert to u64"),
+            .expect("I'm afraid it is not possible to convert this value to u64, my good sir."),
     }
     //Then feed the rest directly into the cosntructor
 
@@ -103,23 +103,23 @@ fn generate_fen(board: Board) -> String {
 } // Creates a FEN from any given Board struct. Inverse function for parse_fen().
 
 fn get_board_coords (algebraic_notation: String) -> Vec<i32> {
-    let row_names = "hgfedcba".to_string();
-    let row_number_index = row_names.chars().position(
-        |x| 
-            x == algebraic_notation
+    let col_names = "abcdefgh".to_string();
+    let col_number_index = col_names.chars().position(
+        |y| 
+            y == algebraic_notation
                 .chars()
                 .nth(0)
                 .unwrap()
-    ).expect("Unable to find row number");
+    ).expect("Unable to find col number");
 
-    let row_number = i32::try_from(row_number_index).expect("Row number index too large");
+    let col_number = i32::try_from(col_number_index).expect("Row number index too large");
 
-    let col_number = algebraic_notation
+    let row_number = algebraic_notation
                 .chars()
                 .nth(1)
                 .unwrap()
                 as i32;
-    return vec![row_number, col_number - 1]
+    return vec![row_number, 8 - col_number]
 } // Generates Board.game_state coords from algebraic notation. [0~7, 0~7]. [0, 0] corresponds to h1, and [8, 8] is a8. 
 
 fn get_algebaric_notation (coords: Vec<i32>) {
@@ -135,9 +135,14 @@ fn set_piece(mut board: Board, coords: &Vec<i32>, piece: char) -> Board {
     return board
 } // changes the given board coordinate to the given piece. 
 
-fn move_piece(mut board: Board, source: String, target: String) -> Board { //Function assumes valid algebraic notation and valid move
+fn move_piece(mut board: Board, source: Vec<i32>, target: Vec<i32>) -> Board { //Function assumes valid board coordinates and valid move 
+    /*
     let source_coords = get_board_coords(source);
     let target_coords = get_board_coords(target);
+    */
+    let source_coords = source;
+    let target_coords = target; // the preceding 2 lines of code stem from laziness
+
     let mut increment_halfmove_counter = true;
     let piece = get_piece(&board, &source_coords);  // target square isn't empty => Capture 
     if get_piece(&board, &target_coords) != '*' 
@@ -147,28 +152,153 @@ fn move_piece(mut board: Board, source: String, target: String) -> Board { //Fun
     }
     if increment_halfmove_counter {
         board.halfmove_counter += 1
+    } else {
+        board.halfmove_counter = 0
     }
     board
 } // Moves a piece to a target square, and updates necessary counters on the board
 
-fn get_piece_movements(coords: Vec<i32>, piece: &char) -> Vec<Vec<i32>> {
+fn get_piece_movements (board: &Board, coords: &Vec<i32>, piece: &char) -> Vec<Vec<i32>> { 
     let mut move_list = vec![];
     let x_pos = coords[0];
-    let y_pos = coords [1];
+    let y_pos = coords[1];
     match piece.to_ascii_lowercase() {
         'p' => todo!(), // The pawn moves straight forward (y+1) if it's not a capture, moves diagonally ([x+1, y+1], [x+1, y-1]) if it's a capture, 
         'b' => todo!(), // The bishop moves along diagonals [+x, +y], [-x, +y], [-x, -y] and [+x, -y], until it hits a piece.
         'n' => todo!(), // the knight teleports to specific relative coordinates [x+-2, y+-1], [x+-1, y+-2]
 
         'r' => { // The rook moves in rows and cols [+-x], [+-y], until it hits a piece. 
+            for new_x in {0..x_pos}.rev() { 
 
+                if board.active_player == 'w' && // breaks at friendly pieces before adding the associated coordinate to the piece's move list
+                board.board_state[new_x as usize][y_pos as usize].is_ascii_uppercase() {
+
+                    break;
+
+                } else if board.active_player == 'b' && 
+                board.board_state[new_x as usize][y_pos as usize].is_ascii_lowercase(){
+
+                    break;
+
+                }
+
+                move_list.push(vec![new_x, y_pos]); // adds the current coordinate to the move list
+
+                if board.active_player == 'w' && // breaks at enemy pieces after adding the associated coordinate to the piece's move list
+                board.board_state[new_x as usize][y_pos as usize].is_ascii_lowercase() {
+
+                    break;
+
+                } else if board.active_player == 'b' && 
+                board.board_state[new_x as usize][y_pos as usize].is_ascii_uppercase(){
+
+                    break;
+
+                }
+
+            } // checks for available moves in -x until we hit a friendly piece (exclusive) or until we hit an enemy piece (inclusive)
+
+            for new_x in x_pos+1..8 { 
+
+                if board.active_player == 'w' && // breaks at friendly pieces before adding the associated coordinate to the piece's move list
+                board.board_state[new_x as usize][y_pos as usize].is_ascii_uppercase() {
+
+                    break;
+
+                } else if board.active_player == 'b' && 
+                board.board_state[new_x as usize][y_pos as usize].is_ascii_lowercase(){
+
+                    break;
+
+                }
+
+                move_list.push(vec![new_x, y_pos]); // adds the current coordinate to the move list
+
+                if board.active_player == 'w' && // breaks at enemy pieces after adding the associated coordinate to the piece's move list
+                board.board_state[new_x as usize][y_pos as usize].is_ascii_lowercase() {
+
+                    break;
+
+                } else if board.active_player == 'b' && 
+                board.board_state[new_x as usize][y_pos as usize].is_ascii_uppercase(){
+
+                    break;
+
+                }
+
+            } // checks for available moves in +x until we hit a friendly piece (exclusive) or until we hit an enemy piece (inclusive)
+
+            for new_y in {0..y_pos}.rev() { 
+
+                if board.active_player == 'w' && // breaks at friendly pieces before adding the associated coordinate to the piece's move list
+                board.board_state[x_pos as usize][new_y as usize].is_ascii_uppercase() {
+
+                    break;
+
+                } else if board.active_player == 'b' && 
+                board.board_state[x_pos as usize][new_y as usize].is_ascii_lowercase(){
+
+                    break;
+
+                }
+
+                move_list.push(vec![x_pos, y_pos]); // adds the current coordinate to the move list (doesn't proc if the loop is broken in the block before)
+
+                if board.active_player == 'w' && // breaks at enemy pieces after adding the associated coordinate to the piece's move list
+                board.board_state[x_pos as usize][new_y as usize].is_ascii_lowercase() {
+
+                    break;
+
+                } else if board.active_player == 'b' && 
+                board.board_state[x_pos as usize][new_y as usize].is_ascii_uppercase(){
+
+                    break;
+
+                }
+
+            } // checks for available moves in -y until we hit a friendly piece (exclusive) or until we hit an enemy piece (inclusive)
+
+            for new_y in y_pos+1..8 { 
+
+                if board.active_player == 'w' && // breaks at friendly pieces before adding the associated coordinate to the piece's move list
+                board.board_state[x_pos as usize][new_y as usize].is_ascii_uppercase() {
+
+                    break;
+
+                } else if board.active_player == 'b' && 
+                board.board_state[x_pos as usize][new_y as usize].is_ascii_lowercase(){
+
+                    break;
+
+                }
+
+                move_list.push(vec![x_pos, y_pos]); // adds the current coordinate to the move list (doesn't proc if the loop is broken in the block before)
+
+                if board.active_player == 'w' && // breaks at enemy pieces after adding the associated coordinate to the piece's move list
+                board.board_state[x_pos as usize][new_y as usize].is_ascii_lowercase() {
+
+                    break;
+
+                } else if board.active_player == 'b' && 
+                board.board_state[x_pos as usize][new_y as usize].is_ascii_uppercase(){
+
+                    break;
+
+                }
+
+            } // checks for available moves in +y until we hit a friendly piece (exclusive) or until we hit an enemy piece (inclusive)
+
+            move_list
         },
         'q' => todo!(), // the queen moves in rows and cols [+-x], [+-y], and along diagonals [+x, +y], [-x, +y], [-x, -y] and [+x, -y], until it hits a piece.
+        // TODO: Once you're finished with Bishop and Rook movement, this should be trivial. 
         'k' => todo!(), // the king teleports to surrounding squares. [x+-1, y+-1].
         '*' => vec![], // the empty square can't move.
-        _ => panic!("Non-filled square on board"),
+        _ => panic!("By God! A non-filled square on board! PANIC!"),
     }
-} //For a given piece on a given coordinate, return a Vec of coordinates that this piece can move to.
+} //For a given piece on a given coordinate, return a Vec of coordinates that this piece can move to. Does not process game flag statuses.
+// giant match-case statement which returns a set of moves for each piece
+
 
 fn active_player_is_in_check(board: Board) -> bool {
     todo!()
@@ -177,18 +307,15 @@ fn active_player_is_in_check(board: Board) -> bool {
 
 
 /*****************************
-*       PUBLIC STRUCTS       *
-*       BEGIN HERE           *
+*  ENUMS                     *
+*  BEGIN HERE                *
 *****************************/
 
-pub enum Pieces {
-    Pawn, 
-    Bishop,
-    Knight, 
-    Rook,
-    Queen,
-    King,
-}
+/*****************************
+*  PUBLIC STRUCTS            *
+*  BEGIN HERE                *
+*****************************/
+
 #[derive(Clone, PartialEq)] // I have no idea how to implement these traits (except for Debug), need to ask for / find walkthrough
 pub struct Game {
     fen: String,
@@ -198,11 +325,15 @@ pub struct Game {
     */
 }
 impl Game {
-    pub fn new() -> Game {
+    pub fn new_from_fen(fen: String) -> Game {
+        let board = parse_fen(&fen);
         Game {
-            fen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string(),
-            board: parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"),
+            fen,
+            board,
         }
+    }
+    pub fn new() -> Game {
+        return Self::new_from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string())
     }
     pub fn get_available_moves(board: Board, active_color: char) -> HashMap<Vec<i32>, Vec<Vec<i32>>>{
         let mut output = HashMap::new();
@@ -211,14 +342,14 @@ impl Game {
                 if active_color == 'w' {
                     if piece.is_ascii_uppercase() { // WHITE pieces are represented by uppercase letters
                         let coords: Vec<i32> = vec![i32::try_from(x_pos).unwrap(), i32::try_from(y_pos).unwrap()];
-                        let movements = get_piece_movements(&coords, &piece);
-                        output.insert(&coords, movements);
+                        let movements = get_piece_movements(&board, &coords, &piece);
+                        output.insert(coords, movements);
                     }
                 } else if active_color == 'b' {
                     if piece.is_ascii_lowercase() { // black pieces are represented by lowercase letters
                         let coords: Vec<i32> = vec![i32::try_from(x_pos).unwrap(), i32::try_from(y_pos).unwrap()];
-                        let movements = get_piece_movements(&coords, &piece);
-                        output.insert(&coords, movements);
+                        let movements = get_piece_movements(&board, &coords, &piece);
+                        output.insert(coords, movements);
                     }
 
                 }
@@ -228,11 +359,13 @@ impl Game {
     } // For any given color, finds pieces of that color. Returns a 
     //Hashmap of coords with pieces of that color, and available moves for each coordinate. 
     
-    pub fn make_move(&mut self, source: <Vec<i32>>, target: <Vec<i32>>) { //Assuming both square and target are Board coordinates <Vec<i32>> with length 2
-        let available_moves = Self::get_available_moves(self.board, self.board.active_player);
+    pub fn make_move(&mut self, source: Vec<i32>, target: Vec<i32>) { //Assuming both square and target are Board coordinates <Vec<i32>> with length 2
+        let available_moves = Self::get_available_moves(self.board.clone(), self.board.active_player);
         if available_moves.contains_key(&source) {
-            self.board = move_piece(self.board, source, target);
-            self.fen = generate_fen(self.board);
+            if available_moves[&source].contains(&target) { // hopefully error free way of checking if the move is a valid move as dicteted by get_avialable_moves()
+                self.board = move_piece(self.board.clone(), source, target);
+                self.fen = generate_fen(self.board.clone());
+            }
         }
     } // Make move if move is available for the active player, 
 }
@@ -275,7 +408,7 @@ impl Board{
 }
 impl fmt::Debug for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut output = "Current board (top left corner is a1)".to_string();
+        let mut output = "Current board (top left corner is a8)".to_string();
         for i in &self.board_state {
             output = format!("{} \n {:?}", output, i);
         } // beautify the printed Vector
